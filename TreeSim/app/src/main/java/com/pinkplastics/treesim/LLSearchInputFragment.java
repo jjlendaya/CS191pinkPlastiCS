67ad805c970a760bb490981bcceca3b06f3dfdbf
jjlendaya <jjlendaya@gmail.com>
2015-03-12 03:14:23 +0800

    Create dialog box and input for Search Node.
    Pressing the Search Node button will now show a dialog box that gets an input for the search key. Max value is 99 and min value is 0. Options for the dialog box are "OK" and "Cancel".

---
+++ TreeSim/app/src/main/java/com/pinkplastics/treesim/LLSearchInputFragment.java
@@ -0,0 +1,37 @@
+package com.pinkplastics.treesim;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.NumberPicker;
+
+/**
+ * Created by Jeynald on 3/12/2015.
+ */
+
+
+public class LLSearchInputFragment extends DialogFragment {
+
+     NumberPicker searchKeyPicker;
+
+     @Override
+     public Dialog onCreateDialog(Bundle savedInstanceState) {
+          View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_number_picker, null);
+          searchKeyPicker = (NumberPicker) v.findViewById(R.id.dialog_numberPicker);
+          searchKeyPicker.setMinValue(0);
+          searchKeyPicker.setMaxValue(99);
+
+          return new AlertDialog.Builder(getActivity(), AlertDialog.THEME_HOLO_LIGHT)
+                  .setTitle(R.string.dialog_ll_search_node)
+                  .setPositiveButton(android.R.string.ok, null)
+                  .setNegativeButton(android.R.string.cancel, null)
+                  .setView(v)
+                  .create();
+     }
+
+     public int getInputValue() {
+          return searchKeyPicker.getValue();
+     }
+}
